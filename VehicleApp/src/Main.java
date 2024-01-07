37572f47d3812777c9e6359cab7c60080b0b550b
Abhishek Patel <abhishekpatel1238@gmail.com>
2024-01-07 07:32:43 +0530

    Creating Vehicle App Project Using Inheritance | Core Java and OOPs

---
+++ VehicleApp/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SUV fortuner = new SUV("Fortuner", false);
+        fortuner.move(40,0);
+        fortuner.accelerate(10);
+        //fortuner.accelerate(-60);
+        System.out.println("Current Gear : "+ fortuner.getCurrentGear());
+        System.out.println("Current Speed : "+ fortuner.getCurrentSpeed());
+    }
+}
